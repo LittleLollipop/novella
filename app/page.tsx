55116081bf41e4147626de8e6821f5c27f85e7ee
Sai <kldtks@live.com>
2024-02-03 15:34:24 +0800

    test palworld

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,31 @@
 import { Editor } from "novel";
 
 export default function Home() {
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Editor />
+
+      <div>
+      <div class="my-10 text-5xl font-extrabold">
+        <span class="animate-pulse bg-gradient-to-r from-pink-500 via-green-500 to-violet-500 bg-clip-text text-transparent">Welcome to Palworld server in Terminus </span>
+      </div>
+      <div>
+      Server Address:<span> palworld.jksdgb.snowinning.com:31211</span>
+      </div>
+      <div>
+      Password: k3HkW1lLlgtW
+      </div>
+      <img src="https://cdn.akamai.steamstatic.com/steam/apps/1623730/capsule_616x353.jpg?t=1705662211" />
+      </div>
+
+      {/* <div className="animate-rainbow">
+        Hello, Terminus!
+      </div> */}
+      {/* <Editor /> */}
+
+
+
+
     </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,19 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      keyframes: {
+        rainbow: {
+          '0%': { 'background-image': 'linear-gradient(to right, red, orange)' },
+          '20%': { 'background-image': 'linear-gradient(to right, orange, yellow)' },
+          '40%': { 'background-image': 'linear-gradient(to right, yellow, green)' },
+          '60%': { 'background-image': 'linear-gradient(to right, green, blue)' },
+          '80%': { 'background-image': 'linear-gradient(to right, blue, violet)' },
+          '100%': { 'background-image': 'linear-gradient(to right, violet, red)' },
+        }
+      },
+      animation: {
+        rainbow: 'rainbow 2s infinite',
+      }
     },
   },
   plugins: [],
